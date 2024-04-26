4c6c5890ff39d3659a08629c4f2a86ad8111bc20
HoangTran18072 <tranhuuhoang692@gmail.com>
2024-04-26 21:32:00 +0700

    firs

---
+++ AutoTele/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoTele
+{
+    public partial class Form1 : Form
+    {
+        String deviceID = "R9JN60HGC4J";
+        Bitmap tele = (Bitmap) Image.FromFile("data//tele.png");
+        List<Bitmap> numbers = ToolHeper.LoadImagesFromDirectory("data//numbers");
+        Bitmap arrow = (Bitmap) Image.FromFile("data//arrow.png");
+        Bitmap _checked = (Bitmap) Image.FromFile("data//checked.png");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //// click tele icon
+            //try
+            //{
+            //    Bitmap screen = KAutoHelper.ADBHelper.ScreenShoot(deviceID);
+            //    Point p = (Point)ToolHeper.FindOutPoint(screen, tele, 0.9);
+            //    if (p != null)
+            //    {
+            //        KAutoHelper.ADBHelper.Tap(deviceID, p.X, p.Y);
+            //    }
+            //}catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //// input number
+            //try
+            //{
+            //    String number = "12323423";
+            //    Bitmap screen = KAutoHelper.ADBHelper.ScreenShoot(deviceID);
+            //    if(ToolHeper.ClickByImage(deviceID, screen, numbers, number))
+            //    {
+            //        Console.WriteLine("input text success");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("input text failed");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            // tap in arrow icon 
+            try
+            {
+                Bitmap screen = KAutoHelper.ADBHelper.ScreenShoot(deviceID);
+                Point p = (Point)ToolHeper.FindOutPoint(screen, /*arrow*/_checked, 0.9);
+                if (p != null)
+                {
+                    KAutoHelper.ADBHelper.Tap(deviceID, p.X, p.Y);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private List<string> GetListUsernam(String dir)
+        {
+            
+            try
+            {
+                string[] lines = File.ReadAllLines(dir);
+                return lines.ToList();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: File not found - " + dir);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading file: " + e.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ AutoTele/ToolHeper.cs
@@ -0,0 +1,90 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AutoTele
+{
+    internal class ToolHeper
+    {
+        public static Point? FindOutPoint(Bitmap mainBitmap, Bitmap subBitmap, double percent = 0.9)
+        {
+            if (subBitmap == null || mainBitmap == null)
+            {
+                return null;
+            }
+
+            if (subBitmap.Width > mainBitmap.Width || subBitmap.Height > mainBitmap.Height)
+            {
+                return null;
+            }
+
+            Image<Bgr, byte> image = new Image<Bgr, byte>(mainBitmap);
+            Image<Bgr, byte> template = new Image<Bgr, byte>(subBitmap);
+            Point? result = null;
+            using (Image<Gray, float> image2 = image.MatchTemplate(template, TemplateMatchingType.CcoeffNormed))
+            {
+                image2.MinMax(out var _, out var maxValues, out var _, out var maxLocations);
+                if (maxValues[0] > percent)
+                {
+                    result = maxLocations[0];
+                }
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            return result;
+        }
+
+
+        public static List<Bitmap> LoadImagesFromDirectory(string directoryPath)
+        {
+            List<Bitmap> images = new List<Bitmap>();
+
+            try
+            {
+                foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*.png", SearchOption.AllDirectories))
+                {
+                    Bitmap image = new Bitmap(filePath);
+                    images.Add(image);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading images: " + e.Message);
+            }
+
+            return images;
+        }
+
+        public static bool ClickByImage(string deviceID, Bitmap screen, List<Bitmap> bitmap, String inputs)
+        {
+            try
+            {
+                foreach (char c in inputs)
+                {
+                    int digit = int.Parse(char.ToString(c));
+                    Bitmap numberBitmap = bitmap[digit];
+                    Point p = (Point)ToolHeper.FindOutPoint(screen, numberBitmap, 0.9);
+                    if (p != null)
+                    {
+                        KAutoHelper.ADBHelper.Tap(deviceID, p.X, p.Y);
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+    }
+}
